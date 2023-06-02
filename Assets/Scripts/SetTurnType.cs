4a93fcbdf1f41e3d5bb61b012100bc09d9fac364
0rb1tZz <denis.joest@gmail.com>
2023-06-02 07:20:47 +0200

    simple menu; simple object interaction

---
+++ Assets/Scripts/SetTurnType.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class SetTurnType : MonoBehaviour
+{
+    public ActionBasedContinuousTurnProvider continuousTurn;
+    public ActionBasedSnapTurnProvider snapTurn;
+
+    public void setTypeFromIndex(int index){
+        if(index == 0){
+            snapTurn.enabled = false;
+            continuousTurn.enabled = true;
+        } else if (index == 1){
+            snapTurn.enabled = true;
+            continuousTurn.enabled = false;
+        }
+    }
+}

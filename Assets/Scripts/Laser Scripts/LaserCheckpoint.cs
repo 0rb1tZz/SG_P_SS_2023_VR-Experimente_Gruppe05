2a96414a45d5fc62690859894819594a8990466a
SmashedPxl <25687659+SmashedPxl@users.noreply.github.com>
2023-07-03 17:20:20 +0200

    Laser checkpoints now change colors when hit

--- Assets/Scripts/Laser Scripts/LaserCheckpoint.cs
+++ Assets/Scripts/Laser Scripts/LaserCheckpoint.cs
@@ -7,25 +7,42 @@ public class LaserCheckpoint : MonoBehaviour
 {
 
     public float acceptedWavelength;
+    private float time = 0.1f;
+    private float resetTime = 0.1f;
+
+    private Color glassColor;
 
     // Start is called before the first frame update
     void Start()
     {
         Color color = LaserHelperFunctions.RgbFromWavelength(acceptedWavelength);
-        color.a = 0.4f;
 
-        Material material = gameObject.transform.GetChild(0).GetComponent<Renderer>().material;
+        Material material = gameObject.transform.GetChild(1).GetComponent<Renderer>().material;
         material.color = color;
+
+        glassColor = gameObject.transform.GetChild(0).GetComponent<Renderer>().material.color;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        time += Time.deltaTime;
+
+        Renderer glassRenderer = gameObject.transform.GetChild(0).GetComponent<Renderer>();
+        if (time < resetTime)
+        {
+            Color color = LaserHelperFunctions.RgbFromWavelength(acceptedWavelength);
+            color.a = 0.5f;
+            glassRenderer.material.color = color;
+        }
+        else
+        {
+            glassRenderer.material.color = glassColor;
+        }
     }
 
     void HitByLaser()
     {
-
+        time = 0;
     }
 }

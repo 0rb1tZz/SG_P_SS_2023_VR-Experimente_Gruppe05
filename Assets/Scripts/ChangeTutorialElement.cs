5b46d697b0949a14679c6722ab5aa6394993f0ac
JackDaniel003 <daniel.joest@t-online.de>
2023-08-02 01:16:45 +0200

    Implemented tutorial menu

---
+++ Assets/Scripts/ChangeTutorialElement.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class ChangeTutorialElement : MonoBehaviour
+{
+    public GameObject menu;
+    private List<GameObject> tutorialList = new List<GameObject>();
+    private int currentIndex = 0;
+    private GameObject currentTutorial;
+    private float time = 0f;
+
+    void Start(){
+        Transform allTutorials = this.transform.GetChild(2);
+        for(int i = 0; i < allTutorials.childCount; i++){
+            tutorialList.Add(allTutorials.GetChild(i).gameObject);
+        }
+
+        List<string> optionList = new List<string>();
+        foreach(GameObject tut in tutorialList){
+            optionList.Add(tut.name);
+        }
+        this.transform.GetChild(3).GetComponent<TMP_Dropdown>().AddOptions(optionList);
+    }
+
+    public void ChangeTutorialToIndex(int index){
+        tutorialList[currentIndex].SetActive(false);
+
+        currentIndex = index;
+
+        tutorialList[currentIndex].SetActive(true);
+    }
+
+    public void ChangeTutorialIndexBy(int changeValue){
+        tutorialList[currentIndex].SetActive(false);
+
+        currentIndex = (currentIndex + tutorialList.Count + changeValue) % tutorialList.Count;
+
+        tutorialList[currentIndex].SetActive(true);
+    }
+
+    public void GoBackToMenu(){
+        this.gameObject.SetActive(false);
+        menu.SetActive(true);
+    }
+}
